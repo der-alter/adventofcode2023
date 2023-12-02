01f17b45d51ba8b9509938247581eb11205ac6d4
Steven J <sjehann@gmail.com>
2023-12-02 23:58:29 +0100

    day 1 part 2

--- bin/day01.ml
+++ bin/day01.ml
@@ -1,22 +1,50 @@
 open Core
 
-let rec fst_digit_exn xs = 
-    match xs with
-    | '1'..'9'::_ -> List.hd xs
-    | _::tl -> fst_digit_exn tl
-    | [] -> Some('0')
+let rec keep = function
+  | [] -> []
+  | hd :: tl -> (
+    match hd with
+    | Some x -> Char.to_string x :: keep tl
+    | None -> keep tl)
 
-let find_calibration s = 
-  let xs = String.to_list s in
-  let fst = Option.value_exn (fst_digit_exn xs) in
-  let lst = Option.value_exn (fst_digit_exn (List.rev xs)) in
-  let num = (String.of_char fst) ^ (String.of_char lst) in
-  Int.of_string num
+let find_calibration s =
+  let sa = String.to_array s in
+  let sa =
+    Array.map sa ~f:(fun x ->
+        match x with
+        | '1' .. '9' -> Some x
+        | _ -> None)
+  in
+  let patterns =
+    [ "one", '1';
+      "two", '2';
+      "three", '3';
+      "four", '4';
+      "five", '5';
+      "six", '6';
+      "seven", '7';
+      "eight", '8';
+      "nine", '9' ]
+  in
+  let () =
+    List.iter patterns ~f:(fun (pattern, digit) ->
+        let found = String.substr_index_all s ~pattern ~may_overlap:true in
+        List.iter found ~f:(fun x -> sa.(x) <- Some digit))
+  in
+  let digits = Array.to_list sa in
+  let all = keep digits in
+  match all with
+  | [] -> 0
+  (* ^ TODO: the list may be empty, why? *)
+  | xs ->
+    let fst = List.hd_exn xs in
+    let lst = List.hd_exn (List.rev xs) in
+    Int.of_string (fst ^ lst)
 
-let sum lst = List.fold lst ~init:0 ~f:(+)
+let sum lst = List.fold lst ~init:0 ~f:( + )
 
 let () =
   let input = In_channel.read_all "day01_input.txt" in
   let xs = String.split input ~on:'\n' in
-  let calibrations = (List.map xs ~f:find_calibration) in
+  let calibrations = List.map xs ~f:find_calibration in
   printf "\n%d\n" (sum calibrations)
